edb13345e082026c7c6e97d3bd69613213f9374b
Kyle Corbin <kylecorbin26@gmail.com>
2020-05-13 22:37:55 -0400

    Got the tag styling just how I want it. I'll need to keep the filter functionality separate, but so far it looks how I envisioned

--- src/comp/Portfolio.js
+++ src/comp/Portfolio.js
@@ -2,14 +2,37 @@ import React from 'react'
 import {Jumbotron, Container, Badge, } from "reactstrap"
 const Portfolio = ()=>{
 
+    const changeActive = (e)=>{
+        let former = document.querySelector('.activee')
+
+        if(former){
+            former.classList.remove('activee')
+            //differinciating between child and parent
+            if(e.target.classList.length ===0){
+                let parent = e.target.parentNode
+                parent.classList.add('activee')
+            }else{
+                e.target.classList.add('activee')
+            }
+        }else{
+            //differinciating between child and parent
+            if(!e.target.classList.length ===0){
+                let parent = e.target.parentNode
+                parent.classList.add('activee')
+            }else{
+                e.target.classList.add('activee')
+            }
+        }
+    }
+
     return(
         <div>
             <Jumbotron>
                 <h1>Web Developer Portfolio</h1>
                 <p>Full Stack, and Front end. I've used React/Redux, Node.js/Express.js, Ant-Design/Reactstrap, Postgresql,sqlite, Vanilla CSS, </p>
                 <div>
-                    <Badge onClick={()=>console.log('Show all')} color="secondary"><span>Show all</span></Badge>
-                    <Badge color="secondary"><span>Front-end</span></Badge>
+                    <Badge onClick={(e)=>changeActive(e)} color="secondary"><span className="activee">Show all</span></Badge>
+                    <Badge onClick={(e)=>changeActive(e)} color="secondary"><span>Front-end</span></Badge>
                     <Badge color="secondary"><span>Back-end</span></Badge>
                     <Badge color="secondary"><span>Full-Stack</span></Badge>
                     <Badge color="secondary"><span>React.js</span></Badge>
