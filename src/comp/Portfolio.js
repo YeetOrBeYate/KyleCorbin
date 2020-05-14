66a11abb196f52b63948a021af138cd2be379ad0
Kyle Corbin <kylecorbin26@gmail.com>
2020-05-13 22:48:21 -0400

    Fixed the zIndex of the nav bar so that the tags dont go ontop of it

--- src/comp/Portfolio.js
+++ src/comp/Portfolio.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Jumbotron, Container, Badge, } from "reactstrap"
+import {Jumbotron, Badge } from "reactstrap"
 const Portfolio = ()=>{
 
     const changeActive = (e)=>{
@@ -29,21 +29,22 @@ const Portfolio = ()=>{
         <div>
             <Jumbotron>
                 <h1>Web Developer Portfolio</h1>
-                <p>Full Stack, and Front end. I've used React/Redux, Node.js/Express.js, Ant-Design/Reactstrap, Postgresql,sqlite, Vanilla CSS, </p>
+                <p>Full Stack, and Front end. I've used React/Redux, Node.js/Express.js,
+                 Ant-Design/Reactstrap, Postgresql,sqlite, Vanilla CSS.</p>
                 <div>
-                    <Badge onClick={(e)=>changeActive(e)} color="secondary"><span className="activee">Show all</span></Badge>
+                    <Badge onClick={(e)=>changeActive(e)} color="secondary"><span>Show all</span></Badge>
                     <Badge onClick={(e)=>changeActive(e)} color="secondary"><span>Front-end</span></Badge>
-                    <Badge color="secondary"><span>Back-end</span></Badge>
-                    <Badge color="secondary"><span>Full-Stack</span></Badge>
-                    <Badge color="secondary"><span>React.js</span></Badge>
-                    <Badge color="secondary"><span>Redux</span></Badge>
-                    <Badge color="secondary"><span>Node.js</span></Badge>
-                    <Badge color="secondary"><span>Express.js</span></Badge>
-                    <Badge color="secondary"><span>Ant-Design</span></Badge>
-                    <Badge color="secondary"><span>Reactstrap</span></Badge>
-                    <Badge color="secondary"><span>Postgresql</span></Badge>
-                    <Badge color="secondary"><span>Sqlite</span></Badge>
-                    <Badge color="secondary"><span>CSS</span></Badge>
+                    <Badge onClick={(e)=>changeActive(e)} color="secondary"><span>Back-end</span></Badge>
+                    <Badge onClick={(e)=>changeActive(e)} color="secondary"><span>Full-Stack</span></Badge>
+                    <Badge onClick={(e)=>changeActive(e)} color="secondary"><span>React.js</span></Badge>
+                    <Badge onClick={(e)=>changeActive(e)} color="secondary"><span>Redux</span></Badge>
+                    <Badge onClick={(e)=>changeActive(e)} color="secondary"><span>Node.js</span></Badge>
+                    <Badge onClick={(e)=>changeActive(e)} color="secondary"><span>Express.js</span></Badge>
+                    <Badge onClick={(e)=>changeActive(e)} color="secondary"><span>Ant-Design</span></Badge>
+                    <Badge onClick={(e)=>changeActive(e)} color="secondary"><span>Reactstrap</span></Badge>
+                    <Badge onClick={(e)=>changeActive(e)} color="secondary"><span>Postgresql</span></Badge>
+                    <Badge onClick={(e)=>changeActive(e)} color="secondary"><span>Sqlite</span></Badge>
+                    <Badge onClick={(e)=>changeActive(e)} color="secondary"><span>CSS</span></Badge>
                 </div>
             </Jumbotron>
         </div>
