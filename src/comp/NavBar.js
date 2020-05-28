51080200dece28548a7061f5ba85a0a0783b9e5c
Kyle Corbin <kylecorbin26@gmail.com>
2020-05-28 16:25:20 -0400

    Fixed the navbar jumping in chrome with a much more efficient solution. Now I'm only toggling a sticky navbar element instead of the original and placeholder one

--- src/comp/NavBar.js
+++ src/comp/NavBar.js
@@ -20,10 +20,7 @@ const NavBarcomp = ()=>{
 
     window.addEventListener("scroll", function(){
 
-        let fake = document.querySelector('.fakeNav');
-        fake.classList.toggle('visible', window.scrollY>100)
-
-        let nav = document.querySelector('.navbar');
+        let nav = document.querySelector('.scrolly');
         nav.classList.toggle('sticky',window.scrollY>100)
 
     })
@@ -49,9 +46,26 @@ const NavBarcomp = ()=>{
                 </Nav>
             </Collapse>
         </Navbar>
-        <div className="fakeNav">
-            placeholderNav
-        </div>
+        {/* this second navbar is the one that sticks to the top of the page onscroll */}
+        <Navbar className="scrolly" color="primary" light expand="md">
+            <NavbarBrand href="/">
+                Kyle Corbin
+            </NavbarBrand>
+            <NavbarToggler onClick={toggleNav} className="mr-2"/>
+            <Collapse isOpen = {isOpen} navbar>
+                <Nav navbar>
+                    <NavItem>
+                        <Link className="nav-link" to="/portfolio">Portfolio</Link>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="https://docs.google.com/document/d/1xhGGkW8qx1XCKsVDJ3AEn8vNRsFYwCC6DsCHddGen8o/edit">Resume</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <Link className="nav-link" to="/contact">Contact</Link>
+                    </NavItem>
+                </Nav>
+            </Collapse>
+        </Navbar>
     </div>
     )
 
