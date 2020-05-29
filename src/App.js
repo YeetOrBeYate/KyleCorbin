9c592cbe755f6e9f09fb156239776a1f03ff7feb
Kyle Corbin <kylecorbin26@gmail.com>
2020-05-29 18:40:08 -0400

    I was making things way too complicated last night. All I needed to do was put the main container and css-doodle displays in a flexBox and adjust the display containers to dynamically take up space

--- src/App.js
+++ src/App.js
@@ -19,20 +19,91 @@ function App() {
 
   const Portfoliostore = useSelector(state=>state.Projects)
 
+  const updateDoodle=()=>{
+    let doodle = document.querySelector('css-doodle');
+
+    doodle.update();
+  }
+
 
 
 
   return (
     <div className="App">
       <NavBarcomp/>
-        <Container className="themed-container" fluid="sm">
-          <Switch>
-            <Route path="/portfolio" component={Portfolio}/>
-            <Route path="/contact" component={Contact}/>
-            <Route path="/project/:id" component={ProjectPage}/>
-            <Route path='/' component={Home}/>
-          </Switch>
-        </Container>
+      <div style={{height:'420px'}} onClick={()=>updateDoodle()}>
+        <css-doodle>{`
+            :doodle {
+              @grid: 18 ;
+              background: #0a0c27;
+              width:100%;
+              height:100%
+            }
+            --hue: calc(180 + 1.5 * @row * @col);
+            background: hsl(var(--hue), 50%, 70%);
+            margin: -.5px;
+            transition: @r(.5s) ease;
+            clip-path: polygon(@pick(
+              '0 0, 100% 0, 100% 100%',
+              '0 0, 100% 0, 0 100%',
+              '0 0, 100% 100%, 0 100%',
+              '100% 0, 100% 100%, 0 100%'
+            ));
+          `}  
+          </css-doodle>
+      </div>
+        <div className="containFlex">
+          <div className="test">
+          {/* <css-doodle>{`
+            :doodle {
+              @grid: 18 ;
+              background: #0a0c27;
+              width:100%;
+              height:100%
+            }
+            --hue: calc(180 + 1.5 * @row * @col);
+            background: hsl(var(--hue), 50%, 70%);
+            margin: -.5px;
+            transition: @r(.5s) ease;
+            clip-path: polygon(@pick(
+              '0 0, 100% 0, 100% 100%',
+              '0 0, 100% 0, 0 100%',
+              '0 0, 100% 100%, 0 100%',
+              '100% 0, 100% 100%, 0 100%'
+            ));
+          `}  
+          </css-doodle> */}
+          </div>
+          <Container className="themed-container" fluid="sm">
+            <Switch>
+              <Route path="/portfolio" component={Portfolio}/>
+              <Route path="/contact" component={Contact}/>
+              <Route path="/project/:id" component={ProjectPage}/>
+              <Route path='/' component={Home}/>
+            </Switch>
+          </Container>
+          <div className="test">
+          {/* <css-doodle>{`
+            :doodle {
+              @grid: 18 ;
+              background: #0a0c27;
+              width:100%;
+              height:100%
+            }
+            --hue: calc(180 + 1.5 * @row * @col);
+            background: hsl(var(--hue), 50%, 70%);
+            margin: -.5px;
+            transition: @r(.5s) ease;
+            clip-path: polygon(@pick(
+              '0 0, 100% 0, 100% 100%',
+              '0 0, 100% 0, 0 100%',
+              '0 0, 100% 100%, 0 100%',
+              '100% 0, 100% 100%, 0 100%'
+            ));
+          `}  
+          </css-doodle> */}
+          </div>
+        </div>
       <FooterBar/>
     </div>
   );
